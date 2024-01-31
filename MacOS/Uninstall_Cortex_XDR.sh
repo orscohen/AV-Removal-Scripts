28e9b60cf31d42f3a83d1d036a2ee6cd1eaf58b3
Or Cohen <136265306+orscohen@users.noreply.github.com>
2024-01-31 16:09:59 +0200

    Update Uninstall_Cortex_XDR.sh

--- MacOS/Uninstall_Cortex_XDR.sh
+++ MacOS/Uninstall_Cortex_XDR.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
-# location of the Cortex XDR app
-uninstaller="/Applications/Cortex XDR Uninstaller.app"
+# Define the location of the Cortex XDR app
+XDRPath="/Applications/Cortex XDR Uninstaller.app"
 
 # Obtain the uninstaller password from the console
 password="Removal_key"
 
 # Run the uninstaller with the provided removal key 
-if "$uninstaller/Contents/Resources/cortex_xdr_uninstaller_tool" "$password"; then
+if "$XDRPath/Contents/Resources/cortex_xdr_uninstaller_tool" "$password"; then
     echo "Cortex XDR uninstallation completed successfully"
 else
     echo "Cortex XDR uninstallation failed"
@@ -16,11 +16,11 @@ fi
 
 # Remove the uninstaller from all locations if Cortex XDR has been deleted
 if [[ ! -d "/Applications/Cortex XDR.app" ]]; then
-    rm -rf "$uninstaller" ||:
+    rm -rf "$XDRPath" ||:
     rm -rf "/Library/Management/PaloAlto" ||:
 else
     echo "Cortex uninstallation failed"
-    chflags nohidden "$uninstaller"
+    chflags nohidden "$XDRPath"
     exit 1
 fi
 
