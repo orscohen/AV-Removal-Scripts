94036533492c2fafbdf0f04fde3f38e8b26fadf4
Or Cohen <136265306+orscohen@users.noreply.github.com>
2024-01-31 16:07:16 +0200

    Update Uninstall_Cortex_XDR.sh

--- MacOS/Uninstall_Cortex_XDR.sh
+++ MacOS/Uninstall_Cortex_XDR.sh
@@ -1,17 +1,30 @@
-#!/bin/bash
+#!/bin/zsh
 
-XDRDirectory="/Applications/Cortex XDR.app/"
+# location of the Cortex XDR app
+uninstaller="/Applications/Cortex XDR Uninstaller.app"
 
-if [[ -d $XDRDirectory ]]; then
+# Obtain the uninstaller password from the console
+password="Removal_key"
 
-    echo "## Cortex XDR Is Installed – Uninstalling Cortex XDR ##"
-    
-    #### Uninstall Script ####
-    sudo /Library/Application\ Support/PaloAltoNetworks/Traps/bin/cortex_xdr_uninstaller_tool  <Uninstalling code>
-    
+# Run the uninstaller with the provided removal key 
+if "$uninstaller/Contents/Resources/cortex_xdr_uninstaller_tool" "$password"; then
+    echo "Cortex XDR uninstallation completed successfully"
 else
-
-    echo "Cortex is Not Installed, aborting XDR removal"
-    exit 0
-
+    echo "Cortex XDR uninstallation failed"
+    exit 1
 fi
+
+# Remove the uninstaller from all locations if Cortex XDR has been deleted
+if [[ ! -d "/Applications/Cortex XDR.app" ]]; then
+    rm -rf "$uninstaller" ||:
+    rm -rf "/Library/Management/PaloAlto" ||:
+else
+    echo "Cortex uninstallation failed"
+    chflags nohidden "$uninstaller"
+    exit 1
+fi
+
+# Attempt to Forget the packages if a match is found
+/usr/sbin/pkgutil --pkgs=com.paloaltonetworks.pkg.cortex && /usr/sbin/pkgutil --forget com.paloaltonetworks.pkg.cortex
+
+echo "Cortex XDR uninstaller script executed successfully"
